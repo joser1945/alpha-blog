abef4bdff6779c2b9a1f5953e57456b363da667f
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-06 13:34:07 -0500

    added users create action and sign up form for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'alpha#home'
   get 'about', to: 'alpha#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
