178164c06b92f27bb7e6b5e71315202aea3d7b3b
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-07 09:02:09 -0500

    added authentication to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article]
     # this renders it to be displayed on the articles browser screen
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to article_path(@article)
