a52d5e3eda74ba3cca50e3b3d4bd147ec36cae88
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-07 15:06:18 -0500

    added create method and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,17 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Ctegory was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -8,5 +19,10 @@ class CategoriesController < ApplicationController
 
   def show
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
 
 end
