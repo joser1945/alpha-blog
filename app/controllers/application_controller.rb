f80416a6b93969190ba719ffe3d4d8282822071b
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-07 10:08:11 -0500

    added restrictions for the user to not be able to edit or update another user and a restriction for users to not edit, delete, and update another users blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     !!current_user
     # !! turns current_user into a bolean
   end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
+  
 end
