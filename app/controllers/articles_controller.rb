5dc18dc14936c2b6be9e4fd51ded9cfd6b8b3471
Jose Rivas <jose.rivas.0410@gmail.com>
2021-04-08 13:15:40 -0500

    modified the articles form to display the categories that it is associated with

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    byebug
     # render plain: params[:article]
     # this renders it to be displayed on the articles browser screen
     @article = Article.new(article_params)
@@ -58,7 +59,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
